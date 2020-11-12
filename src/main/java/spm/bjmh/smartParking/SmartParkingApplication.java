55fa898ab896db6c7a04352117e136a056417f50
miris-mp <miriam.petrocchi94@gmail.com>
2020-11-12 16:47:46 +0100

    changing folder organization of the project

---
+++ src/main/java/spm/bjmh/smartParking/SmartParkingApplication.java
@@ -0,0 +1,14 @@
+package spm.bjmh.smartParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SmartParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spm/bjmh/smartParking/SmartParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package spm.bjmh.smartParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
