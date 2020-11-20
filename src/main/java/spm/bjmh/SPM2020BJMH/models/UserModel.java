509d290f88db8d4c47cc93bacc5d4a39c7dbcc9a
Miriam <miriam.petrocchi94@gmail.com>
2020-11-20 12:00:21 +0100

    Fix to have a main branch

--- src/main/java/spm/bjmh/SPM2020BJMH/models/UserModel.java
+++ src/main/java/spm/bjmh/SPM2020BJMH/models/UserModel.java
@@ -6,9 +6,7 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
