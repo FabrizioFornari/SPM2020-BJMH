1b142f39650040aa0346b325add64a786d507382
Bilel <bilel.braiek@studenti.unicam.it>
2020-12-02 17:55:53 +0200

    Add MessageResponse Model

---
+++ src/main/java/spm/bjmh/SPM2020BJMH/models/MessageResponse.java
@@ -0,0 +1,19 @@
+package spm.bjmh.SPM2020BJMH.models;
+
+public class MessageResponse {
+    private String message;
+
+
+
+public MessageResponse(String message){
+}
+
+
+public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
