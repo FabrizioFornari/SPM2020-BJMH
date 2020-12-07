0832083a47670f910b6115dbff1c270621a0d573
Bilel <bilel.braiek@studenti.unicam.it>
2020-12-07 14:02:20 +0200

    Add UserConstants

---
+++ src/main/java/spm/bjmh/SPM2020BJMH/Constants/UserConstants.java
@@ -0,0 +1,16 @@
+package spm.bjmh.SPM2020BJMH.Constants;
+
+public class UserConstants {
+    public static final String VALID_EMAIL = "mario@smart.com";
+    public static final String INVALID_EMAIL = "invalidEmail";
+    public static final String VALID_PASSWORD = "validPassword";
+    public static final String INVALID_PASSWORD = "";
+    public static final String VALID_PHONE = "+39 332 555555555";
+    public static final String INVALID_PHONE = "45678";
+    public static final String VALID_PLATE = "ABCD0123";
+    public static final String INVALID_PLATE = "0000000000";
+    public static final String USERNAME = "mar10";
+    public static final String FIRSTNAME = "Name";
+    public static final String LASTNAME = "Surname";
+
+}
