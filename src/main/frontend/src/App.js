f0213098e3d7583ee9c8133ef2bc34c7a74a67a9
jerushajosi.manchala <jerushajosi.manchala@studenti.unicam.it>
2020-12-10 21:26:21 +0100

    merge to main branch

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,56 +1,12 @@
-import React from 'react';
-import {Button} from 'antd';
-import 'antd/dist/antd.css';
-import logo from './logo.svg';
 import './App.css';
-import {Switch, Route, Redirect} from 'react-router-dom'
-<<<<<<< HEAD
-import { adminRoute } from './routes/routesfile';
-import Frame from './components/Frame/index.js'
-
+import GoogleMap from './components/GoogleMap';
 function App() {
   return (
-    <Frame>
-      <Switch>
-        {adminRoute.map(route =>{
-=======
-import { adminRoutes } from './routes/routesfile';
-import Frame from './components/Frame/Index.js'
-import { isLogined } from './utils/auth'
-
-function App() {
-  return  isLogined()? (
-    <Frame>
-      <Switch>
-        {adminRoutes.map(route =>{
->>>>>>> us-2-frontend
-          return(
-            <Route
-            key={route.path}
-            path={route.path}
-            exact={route.exact}
-            render={routeProps =>{
-              return <route.component{...routeProps}/>;
-            }}
-            />
-          );
-        })}
-<<<<<<< HEAD
-=======
-        <Redirect to={adminRoutes[0].path} from="/admin" />
->>>>>>> us-2-frontend
-        <Redirect to="/404"/>
-
-      </Switch>
-    </Frame>
-<<<<<<< HEAD
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+    </div>
   );
 }
-=======
-    ): (
-      <Redirect to="/login" />
-    );
-  }
->>>>>>> us-2-frontend
 
 export default App;
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import App from './App';
-import "antd/dist/antd.css";
 import './index.css';
-// import App from './App';
-//import * as serviceWorker from "./serviceWorker";
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { mainRoutes } from './routes/routesfile'
-
 
 ReactDOM.render(
-  // <App/>,
-  <Router>
-    <Switch>
-      <Route path="/admin" render={routeProps=><App {...routeProps}/>}/>
-      {mainRoutes.map(route => {
-        return <Route key={route.path} path={route.path} component={route.component} />
-      })}
-<<<<<<< HEAD
-=======
-      <Redirect to="/admin" from="/" />
->>>>>>> us-2-frontend
-      <Redirect to="/404" />
-    </Switch>,
-     </Router>,
-  //    <React.StrictMode>
-  // </React.StrictMode>,
-
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
