e9dc54f20c28a96b460b711c54367dc88ff4a14e
Bilel <bilel.braiek@studenti.unicam.it>
2020-12-10 18:36:40 +0200

    Fixed Error

--- src/main/java/spm/bjmh/SPM2020BJMH/Constants/RegexConstants.java
+++ src/main/java/spm/bjmh/SPM2020BJMH/Constants/RegexConstants.java
@@ -1,7 +1,7 @@
 package spm.bjmh.SPM2020BJMH.Constants;
 
 public class RegexConstants {
-    public static final String MAIL_REGEX ="[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}";
+    public static final String EMAIL_REGEX ="[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}";
     public static final String PASSWORD_REGEX="((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,10})";
     public static final String USERNAME_REGEX="^[a-z0-9_-]{3,5}$";
     public static final String PHONE_REGEX ="/^([+]39)?((38[{8,9}|0])|(34[{7-9}|0])|(36[6|8|0])|(33[{3-9}|0])|(32[{8,9}]))([\\d]{7})$/";
