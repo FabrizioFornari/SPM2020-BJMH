06f25c640d2ae64df075378dd311309265c6242f
Bilel <bilel.braiek@studenti.unicam.it>
2020-12-10 18:51:46 +0200

    Deleted file to do changes

--- src/main/java/spm/bjmh/SPM2020BJMH/repositories/RoleRepository.java
+++ src/main/java/spm/bjmh/SPM2020BJMH/repositories/RoleRepository.java
@@ -1,13 +1,1 @@
-package spm.bjmh.SPM2020BJMH.repositories;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import spm.bjmh.SPM2020BJMH.models.ERole;
-import spm.bjmh.SPM2020BJMH.models.RoleModel;
-
-import java.util.Optional;
-
-public interface RoleRepository  extends MongoRepository<RoleModel, String> {
-
-    Optional<RoleModel> findByName(ERole name);
-    RoleModel findByRole(String role);
-}
--- src/main/java/spm/bjmh/SPM2020BJMH/repositories/UserRepository.java
+++ src/main/java/spm/bjmh/SPM2020BJMH/repositories/UserRepository.java
@@ -1,13 +1,1 @@
-package spm.bjmh.SPM2020BJMH.repositories;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import spm.bjmh.SPM2020BJMH.models.UserModel;
-
-public interface UserRepository extends MongoRepository<UserModel, String> {
-    // find a user by email
-    UserModel findByEmail(String email);
-
-    UserModel findByUsername(String username);
-    // check the exist of an user
-    UserModel existsByEmail(String email);
-}
